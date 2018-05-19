c763394eee5bab5d144892ff8f7886d7bd66685c
arturjaakman <artur.jaakman@gmail.com>
2018-05-19 21:10:07 +0100

    CRUD

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,7 +1,11 @@
 class Section < ApplicationRecord
 
-  belongs_to :pages
   has_many :section_edits
   has_many :admin_users, :through => :section_edits
-  
+
+  scope :visible lambda { where(:visible => true)}
+  scope :invisible lambda { where(:visible => false)}
+  scope :sorted lambda { order("position ASC") }
+  scope :newest_first lambda { create_at("create_at DESC")}
+
 end
